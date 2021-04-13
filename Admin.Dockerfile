2a8546d9dd2384ac9f47b0c70480c7d086379bc1
Moro-Code <omar@omarnasr.ca>
2021-04-12 23:28:58 -0400

    fix nginx bug with admin & add storage plugin

--- Admin.Dockerfile
+++ Admin.Dockerfile
@@ -14,6 +14,7 @@ RUN npm install && npm run build
 FROM nginx
 
 COPY --from=0 build /usr/share/nginx/html
+COPY ./nginx/conf.d /etc/nginx/conf.d
 
 
 
---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports= ({env}) => {
+  var storage = env("STRAPI_STORAGE", "local")
+  if( storage === "azure"){
+    return {
+      upload: {
+        provider: 'azure-storage',
+        providerOptions: {
+          account: env('STORAGE_ACCOUNT'),
+          accountKey: env('STORAGE_ACCOUNT_KEY'),
+          serviceBaseURL: env('STORAGE_URL'),
+          containerName: env('STORAGE_CONTAINER_NAME'),
+          defaultPath: 'assets',
+          maxConcurrent: 10
+        }
+      }
+    }
+  }
+};
