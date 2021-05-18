6a738805faba8b172be2109c322741cf4256a210
Marcus <mblais93@gmail.com>
2021-05-18 17:03:27 -0400

    updated azure context script

--- terraform/azureSetContext.sh
+++ terraform/azureSetContext.sh
@@ -15,19 +15,8 @@ echo "Setting account"
 az account set --subscription $subscription_id
 echo "Done."
 
-echo "Cleaning Workspace"
-rm -rfd $(find . -name \*.terraform -type d)
-rm  $(find . -name provider.tf -type f)
+echo "Downloading varfile from azure storage account.."
+az storage blob download --account-name $storage_account_name --container-name $application_name --file $TG_VAR_FILE -n variables.json
 echo "Done."
-
-echo "Exporting Terraform Service Principal..."
-export terraform_sp_pass=$(az keyvault secret show --vault-name alphasite-keyvault-dev --name alphasite-terraform-pass-dev --query value -otsv)
-export terraform_sp_id=$(az ad sp list --display-name marcus-terraform-sp-bdm-dev --query [].appId -o tsv)
-echo "Done."
-
-echo "Setting Terraform SP ID and Pass in VarFile"
-sed -i "s/%CLIENT_ID%/$terraform_sp_id/g" $VARFILE
-sed -i "s/%CLIENT_SECRET%/$terraform_sp_pass/g" $VARFILE
-echo "Done"
 
 
