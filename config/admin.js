8b7894a7c77f6fea165a34203e919a9e064b27d1
Kris Charbonneau <char0799@algonquinlive.com>
2022-01-17 10:47:48 -0500

    set JWT expiry to 8h for admin users

--- config/admin.js
+++ config/admin.js
@@ -1,42 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('STRAPI_ADMIN_JWT_SECRET', 'bbc63056fa9b1d5f2582f643d2b38e12'),
-    logo: null
-  },
-  theme: {
-    colors: {
-      alternative100: '#000',
-      alternative200: '#000',
-      alternative500: '#000',
-      alternative600: '#000',
-      alternative700: '#000',
-      danger700: '#000',
-      primary100: '#000',
-      primary200: '#000',
-      primary500: '#000',
-      primary600: '#000',
-      primary700: '#000',
+    options: {
+      expiresIn: "8h",
     },
   },
-  config: {
-    theme: {
-      main: {
-        colors: {
-          alternative100: '#000',
-          alternative200: '#000',
-          alternative500: '#000',
-          alternative600: '#000',
-          alternative700: '#000',
-          danger700: '#000',
-          primary100: '#000',
-          primary200: '#000',
-          primary500: '#000',
-          primary600: '#000',
-          primary700: '#000',
-        },
-      }
-    },
-    bootstrap() {},
-  },
-  bootstrap() {},
 });
