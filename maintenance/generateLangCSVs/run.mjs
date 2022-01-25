eedb793a9b5aefcad71ec8960a3982549de4d5ad
Zain Syed <zeddotes@gmail.com>
2022-01-25 15:45:15 -0500

    - updated the script

--- maintenance/generateLangCSVs/run.mjs
+++ maintenance/generateLangCSVs/run.mjs
@@ -1,10 +1,30 @@
 #!/usr/bin/env zx
 import 'zx/globals'
 
+import { generate, stringify } from 'csv'
+
 const enFilePath = '../../src/admin/extensions/translations/en.json'
 const frFilePath = '../../src/admin/extensions/translations/fr.json'
 
 export default async () => {
   const en = await fs.readFile(enFilePath)
-  console.log(JSON.parse(en))
+  // csv.generate({
+  //   length: 4
+  // }, () => {
+
+  // })
+  var stream = fs.createWriteStream('./build/myFile.csv', { flags: 'a' });
+  stringify([[2, 3], [44, 3]], {
+    quoted: true,
+    header: true,
+    columns: {
+      year: 'birthYear',
+      phone: 'phone'
+    }
+  })
+    .pipe(stream)
+
+  // console.log(xx)
+  // const fr = await fs.readFile(enFilePath)
+  // console.log(JSON.parse(en))
 }
