1b822179731c6a9243cfac6a16f3f6e2665d313f
Zain Syed <zeddotes@gmail.com>
2022-02-10 15:06:05 -0500

    - added csv -> json generator boilerplate

---
+++ maintenance/generateLangJSONs/run.mjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env zx
+import 'zx/globals'
+
+import { stringify } from 'csv'
+import { buildRows } from './util.mjs'
+
+export default async () => {
+  const en = JSON.parse(await fs.readFile('./sources/en.json'))
+  const fr = JSON.parse(await fs.readFile('./sources/fr.json'))
+
+  const mergedAndFlattened = buildRows(en, fr)
+  const stream = fs.createWriteStream(`./build/strapi-translations.csv`, { flags: 'a' });
+
+  stringify(mergedAndFlattened, {
+    quoted: true,
+    header: true,
+    columns: {
+      id: 'ID',
+      en: 'EN',
+      fr: 'FR'
+    }
+  })
+    .pipe(stream)
+}
