e5cc14e10814b9a2ee1002e82bc2ed36f8d1bd63
shawn320 <azuneko1110@gmail.com>
2022-02-14 14:43:18 -0500

    db config

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "0.0.0.0"),
-      port: env.int("DATABASE_PORT", 5432),
+      port: env.int("DATABASE_PORT", 5433),
       database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "postgres"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
       ssl: env.bool("DATABASE_SSL", false),
     },
     debug: false,
