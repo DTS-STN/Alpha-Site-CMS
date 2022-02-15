3d6d884be6eab135cba9263a9f1e305d50ea5f4e
Kris Charbonneau <char0799@algonquinlive.com>
2022-02-15 14:13:09 -0500

    updated changelog, changed db connection back

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', '0.0.0.0'),
       port: env.int('DATABASE_PORT', 5433),
       database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'admin'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: env.bool('DATABASE_SSL', false),
     },
     debug: false,
