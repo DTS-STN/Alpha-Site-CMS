22145b2cc1b2d44c760e950a78c092cee3d14b8e
Zain Syed <zeddotes@gmail.com>
2022-02-21 17:35:30 -0500

    - added email provider code for sendmail and dep

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,32 @@
-module.exports= ({env}) => {
-  var storage = env("STRAPI_STORAGE", "local")
-  if( storage === "azure"){
-    return {
-      upload: {
-        provider: 'azure-storage',
-        providerOptions: {
-          account: env('STORAGE_ACCOUNT'),
-          accountKey: env('STORAGE_ACCOUNT_KEY'),
-          serviceBaseURL: env('STORAGE_URL'),
-          containerName: env('STORAGE_CONTAINER_NAME'),
-          defaultPath: 'assets',
-          maxConcurrent: 10
-        }
+module.exports = ({ env }) => {
+  const emailPlugin = {
+    config: {
+      provider: 'sendmail',
+      settings: {
+        defaultFrom: 'no-reply@servicecanada.gc.ca',
+        defaultReplyTo: 'no-reply@servicecanada.gc.ca',
+      },
+    }
+  }
+
+  let storagePlugin = {}
+  const storage = env("STRAPI_STORAGE", "local")
+  if (storage === "azure") {
+    storagePlugin = {
+      provider: 'azure-storage',
+      providerOptions: {
+        account: env('STORAGE_ACCOUNT'),
+        accountKey: env('STORAGE_ACCOUNT_KEY'),
+        serviceBaseURL: env('STORAGE_URL'),
+        containerName: env('STORAGE_CONTAINER_NAME'),
+        defaultPath: 'assets',
+        maxConcurrent: 10
       }
     }
+  }
+
+  return {
+    upload: storagePlugin,
+    email: emailPlugin
   }
 };
