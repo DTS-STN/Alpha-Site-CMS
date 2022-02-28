061a6f7652e7f7e7ba56d5404b2b53db2b4c66b0
shawn320 <azuneko1110@gmail.com>
2022-02-28 12:38:05 -0500

    update page content

---
+++ src/api/page-content/services/page-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-content service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::page-content.page-content');
