315ee8c2a6e5fa2c02b6bbbeb085e7014c04a553
shawn320 <azuneko1110@gmail.com>
2022-03-30 11:37:09 -0400

    single type

---
+++ src/api/signup-info/controllers/signup-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  signup-info controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::signup-info.signup-info');
---
+++ src/api/signup-info/services/signup-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * signup-info service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::signup-info.signup-info');
